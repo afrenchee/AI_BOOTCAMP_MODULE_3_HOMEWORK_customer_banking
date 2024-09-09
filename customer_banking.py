337a8a90605be3a823165cbd2c4b350f3f45fec1
afrenchee <afrenchee@gmail.com>
2024-09-09 04:15:33 -0400

    upload

--- customer_banking.py
+++ customer_banking.py
@@ -1,29 +1,131 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
+
+import os, sys
+windows_is_the_OS = False # This variable is set by the main programmer to ensure that Terminal / Command Prompt commands are correctly executed in clear_screen() and end_program()
+
+
+
+def clear_screen(): # You best be usin a terminal or powershell for this code!!!
+    if windows_is_the_OS:
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+
+def print_invalid_input(invalid_input):
+    print(f"\n{invalid_input} is not a valid input you silly goose!!!\n")
+
+
+
+def end_program():
+    if windows_is_the_OS:
+        os.system('dir')
+    else:
+        os.system('pwd')
+        os.system('ls')
+    print("")
+    sys.exit()
+
+
 
 # Define the main function
 def main():
-    """This function prompts the user to enter the savings and cd account balance, interest rate,
-    and the length of months to determine the interest gained.
+    """This function prompts the user to enter
+        - the savings and
+        - cd account balance,
+        - interest rate,
+        - and the length of months to determine the interest gained.
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    clear_screen()
+    print("Welcome to PRETEND INTERNATIONAL BANK!\n")
+    while True:
+        user_input_balance = input("Please enter an integer USD balance number for your pretend savings account calculator extrordinare!!! $")
+        if user_input_balance.isdigit():
+            clear_screen()
+            break
+        else:
+            print_invalid_input(user_input_balance)
+    print("Next step!\n")
+    while True:
+        user_input_interest = input("Please enter an integer for the yearly interest rate percent for your pretend savings account calculator extrordinare!!!: ")
+        if user_input_interest.isdigit():
+            clear_screen()
+            break
+        else:
+            print_invalid_input(user_input_interest)
+    print("Almost done!\n")
+    while True:
+        user_input_months = input("Please enter an integer for the number of months invested in your pretend savings account calculator extrordinare!!!: ")
+        if user_input_months.isdigit():
+            clear_screen()
+            break
+        else:
+            print_invalid_input(user_input_months)
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    updated_balance, interest_earned = create_savings_account(user_input_balance, user_input_interest, user_input_months)
 
-    # Print out the interest earned and updated savings account balance with interest earned for the given months.
+    # Print out the interest ecarned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    clear_screen()
+    print("\nWell done User! Now it's time to repeate everything with the CD stuff!")
+
+    print(f"Interest earned: ${interest_earned}")
+    print(f"Updated savings account balance: ${updated_balance}\n")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    #updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    while True:
+        user_input_balance = input("Please enter an integer USD balance number for your pretend CD account calculator extrordinare!!! $")
+        if user_input_balance.isdigit():
+            clear_screen()
+            break
+        else:
+            print_invalid_input(user_input_balance)
+    print("Next step!\n")
+    while True:
+        user_input_interest = input("Please enter an integer for the interest rate percent for your pretend CD account calculator extrordinare!!!: ")
+        if user_input_interest.isdigit():
+            clear_screen()
+            break
+        else:
+            print_invalid_input(user_input_interest)
+    print("Almost done!\n")
+    while True:
+        user_input_months = input("Please enter an integer for the number of months invested in your pretend CD account calculator extrordinare!!!: ")
+        if user_input_months.isdigit():
+            clear_screen()
+            break
+        else:
+            print_invalid_input(user_input_months)
+    updated_balance, interest_earned = create_cd_account(user_input_balance, user_input_interest, user_input_months)
+
+    print("\nWell done User! Here are your CD results\n")
+
+    print(f"Interest earned: ${interest_earned}")
+    print(f"Updated savings account balance: ${updated_balance}\n")
+
+    end_program()
+
+
+
+
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
+
+
+
--- savings_account.py
+++ savings_account.py
@@ -1,5 +1,8 @@
 """Import the Account class from the Account.py file."""
 # ADD YOUR CODE HERE
+from Account import Account
+
+
 
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
@@ -14,21 +17,31 @@ def create_savings_account(balance, interest_rate, months):
         float: The updated savings account balance after adding the interest earned.
         And returns the interest earned.
     """
+
+
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
+    instance_of_account_class = Account(balance, 0) # "You will need to use 0 for the amount of interest to set the balance before you pass the interest earned to the set interest method."
 
     # Calculate interest earned
-     # ADD YOUR CODE HERE
+    # ADD YOUR CODE HERE
+    interest_earned = int(balance) * (int(interest_rate)/100 * int(months)/12)
+    interest_earned = round(interest_earned, 3) # "The values are formatted to two decimal places and thousandths. (6 points)"
 
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
+    updated_balance = int(balance) + (interest_earned)
+    updated_balance = round(updated_balance, 2) # "The values are formatted to two decimal places and thousandths. (6 points)"
 
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
+    instance_of_account_class.set_balance(updated_balance)
 
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
+    instance_of_account_class.set_interest(interest_earned)
 
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    # ADD YOUR CODE HERE
+    return updated_balance, interest_earned
